261bfb9d79b7d3c93e37415ad8258b2d6313fade
Samuel Kurian <samuelrkurian@gmail.com>
2023-05-16 02:04:47 +1200

    factory class for AI difficulties

---
+++ src/main/java/nz/ac/auckland/se281/AIModesFactory.java
@@ -0,0 +1,22 @@
+package nz.ac.auckland.se281;
+
+import nz.ac.auckland.se281.Main.Difficulty;
+import nz.ac.auckland.se281.aiModes.easyAI;
+
+public class AIModesFactory {
+    public static AI getAIMode(Difficulty mode){
+        String nameAI = "Jarvis";
+        switch (mode) {
+            case EASY:
+                return new easyAI();
+            // case "medium":
+            //     return new mediumAI();
+            // case "hard":
+            //     return new hardAI();
+            // case "master":
+            //     return new masterAI();
+            default:
+                return null;
+        }
+    }
+}
