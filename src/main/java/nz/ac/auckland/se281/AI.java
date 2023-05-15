641108b5e6b376bf09b2ce6404fe3809abb318c9
Samuel Kurian <samuelrkurian@gmail.com>
2023-05-16 02:31:05 +1200

    converted AI to abstract class extending Player and engaging strategy

--- src/main/java/nz/ac/auckland/se281/AI.java
+++ src/main/java/nz/ac/auckland/se281/AI.java
@@ -1,5 +1,18 @@
 package nz.ac.auckland.se281;
 
-public interface AI{
-    public int[] play();
+public abstract class AI extends Player{
+    public Strategy strategy;
+    protected int count;
+
+    public AI(String name) {
+        super(name);
+    }
+
+    protected abstract void setStrategy();
+
+    @Override
+    public int[] play() {
+        return this.strategy.engage();
+    }
+
 }
