98301a81000fe615955309b30e56c19cece4c3ce
Samuel Kurian <samuelrkurian@gmail.com>
2023-05-16 02:29:46 +1200

    Player abstract class defined

---
+++ src/main/java/nz/ac/auckland/se281/Player.java
@@ -0,0 +1,25 @@
+package nz.ac.auckland.se281;
+
+public abstract class Player {
+    private String name;
+    private int score;
+
+    public Player(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public void addScore() {
+        this.score++;
+    }
+
+    public abstract int[] play();
+}
