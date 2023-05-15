76cfb2ca6b0f92bacc5def842f18c880ed0e3a74
Samuel Kurian <samuelrkurian@gmail.com>
2023-05-16 02:33:25 +1200

    defined random strategy class

---
+++ src/main/java/nz/ac/auckland/se281/strategies/RandomStrategy.java
@@ -0,0 +1,16 @@
+package nz.ac.auckland.se281.strategies;
+
+import nz.ac.auckland.se281.Strategy;
+import nz.ac.auckland.se281.Utils;
+
+public class RandomStrategy implements Strategy{
+
+    @Override
+    public int[] engage() {
+        int fingers = Utils.getRandomNumber(1, 5);
+        int sum = Utils.getRandomNumber(fingers + 1, 10);
+
+        return new int[]{fingers, sum};
+    }
+    
+}
