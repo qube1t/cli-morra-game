c7263d8ed7208b9ecbc90b8a02c9a9212d98249f
Samuel Kurian <samuelrkurian@gmail.com>
2023-05-16 02:07:59 +1200

    removed user inputs from play(); defined conductPlayerTurn()

--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -5,8 +5,8 @@ import nz.ac.auckland.se281.Main.Difficulty;
 public class Morra {
 
   private int round;
-  private String playerName;
   private AI ai;
+  private Human human;
 
   public Morra() {
     round = -1;
@@ -14,9 +14,18 @@ public class Morra {
 
   public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {
     System.out.println("Welcome " + options[0] + "!");
-    playerName = options[0];
+    human = new Human(options[0]);
     ai = AIModesFactory.getAIMode(difficulty);
     round = 0;
+  }
+
+  private int[] conductPlayerTurn(Player player) {
+    int[] play = player.play();
+    int fingers = play[0];
+    int sum = play[1];
+    MessageCli.PRINT_INFO_HAND.printMessage(player.getName(), Integer.toString(fingers), Integer.toString(sum));
+
+    return play;
   }
 
   public void play() {
@@ -29,37 +38,17 @@ public class Morra {
     MessageCli.START_ROUND.printMessage(Integer.toString(round));
     MessageCli.ASK_INPUT.printMessage();
 
-    int fingers = 0;
-    int sum = 0;
-    int trySuccess = 0;
-    do {
-      String input = Utils.scanner.nextLine();
-      String[] inputArray = input.split(" ");
-
-      try{
-        fingers = Integer.parseInt(inputArray[0]);
-        sum = Integer.parseInt(inputArray[1]);
-
-        if (fingers >= 1 && fingers <= 5 && sum >= 1 && sum <= 10)
-          trySuccess = 1;
-          else
-            MessageCli.INVALID_INPUT.printMessage();
-      } catch (Exception e) {
-        MessageCli.INVALID_INPUT.printMessage();
-      }
-    } while (trySuccess == 0);
-
-    MessageCli.PRINT_INFO_HAND.printMessage(playerName, Integer.toString(fingers), Integer.toString(sum));
+    int[] humanResult = this.conductPlayerTurn(human);
+    int humanFingers = humanResult[0];
+    int humanSum = humanResult[1];
     
-    int[] aiPlay = ai.play();
+    int[] aiResult = this.conductPlayerTurn(ai);
+    int aiFingers = aiResult[0];
+    int aiSum = aiResult[1];
 
-    int aiFingers = aiPlay[0];
-    int aiSum = aiPlay[1];
-    MessageCli.PRINT_INFO_HAND.printMessage("Jarvis", Integer.toString(aiFingers), Integer.toString(aiSum));
-
-    if (fingers + aiFingers == sum) {
+    if (humanFingers + aiFingers == humanSum) {
       MessageCli.PRINT_OUTCOME_ROUND.printMessage("HUMAN_WINS");
-    } else if (fingers + aiFingers == aiSum) {
+    } else if (humanFingers + aiFingers == aiSum) {
       MessageCli.PRINT_OUTCOME_ROUND.printMessage("AI_WINS");
     } else {
       MessageCli.PRINT_OUTCOME_ROUND.printMessage("DRAW");
